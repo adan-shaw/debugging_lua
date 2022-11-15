6ea583a47cd5a60ca0ba092040170501eb09593a
adan shaw <adan_shaw@aliyun.com>
2022-11-15 18:22:18 +0800

    Create def_basic.lua

---
+++ lua_basic/def_basic.lua
@@ -0,0 +1,31 @@
+local dump = _G.string.dump
+local loadstring = _G.loadstring
+
+
+
+--简单粗暴的number交换函数
+local function f_swap_num(a,b) return b,a; end
+api_swap_num = loadstring(dump(f_swap_num))
+
+
+
+return {
+	swap_num = api_swap_num
+
+
+}
+
+
+
+--测试记录
+--[[
+	local swap_num = require("def_basic").swap_num
+	--测试宏函数
+	local x = 999
+	local x2 = 111
+	print(x, x2)
+	x, x2 = swap_num(x, x2)
+	print(x, x2)
+
+
+]]
